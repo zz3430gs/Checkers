8bb85e4b44d6fd3100c48e004d46040035c0071c
zz3430gs <keigolis95@gmail.com>
2016-11-08 14:19:14 -0600

    Added a button to the main activity so that when click it starts the game. Added another activity to start the game. Tried to add in a gridlayout specified the column and rows as 32 by 32

--- app/src/main/java/com/example/zz3430gs/checkers/MainActivity.java
+++ app/src/main/java/com/example/zz3430gs/checkers/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.zz3430gs.checkers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mStartButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mStartButton = (Button)findViewById(R.id.start_game_button);
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent newIntent = new Intent(MainActivity.this, Checkers.class);
+            }
+        });
+
     }
 }
